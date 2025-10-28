16f6afc7fb63c93bfb7c527ffa27806b9f7b3601
Maria-Olaya <mmolayal@eafit.edu.co>
2025-10-28 16:11:59 -0500

    partido

--- src/main/java/com/proyecto/cabapro/controller/NewsController.java
+++ src/main/java/com/proyecto/cabapro/controller/NewsController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.proyecto.cabapro.model.NewsArticle;
 import com.proyecto.cabapro.service.NewsDataService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 @RestController
 public class NewsController {
 
@@ -17,8 +23,20 @@ public class NewsController {
         this.newsDataService = newsDataService;
     }
 
+    @Operation(
+        summary = "Obtiene noticias de NBA",
+        description = "Devuelve solo noticias de NBA en inglés filtradas por categoría deportes",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Lista de noticias de NBA",
+                content = @Content(array = @ArraySchema(schema = @Schema(implementation = NewsArticle.class)))
+            )
+        }
+    )
     @GetMapping("/nba-news")
     public List<NewsArticle> fetchNBAnews() {
         return newsDataService.getBasketballNews();
     }
 }
+
